02e30fe0375f21ad26e48806bc8f7bc0fd9dc921
Mahammad Bakr <49376835+mahammadbakr@users.noreply.github.com>
2023-08-21 16:12:48 +0300

    update phase 4

--- lib/main.dart
+++ lib/main.dart
@@ -66,6 +66,8 @@ class MyApp extends StatelessWidget {
                   ],
                 ),
               ),
+
+              // Create Categries section
               const Align(
                 alignment: Alignment.centerLeft,
                 child: Text(
@@ -78,6 +80,7 @@ class MyApp extends StatelessWidget {
                   ),
                 ),
               ),
+
               SizedBox(
                 height: MediaQuery.of(context).size.height * 0.08,
                 child: ListView(
@@ -125,7 +128,50 @@ class MyApp extends StatelessWidget {
                             'https://tricky-photoshop.com/wp-content/uploads/2017/08/final-1.png'),
                   ],
                 ),
-              )
+              ),
+
+              const SizedBox(
+                height: 20,
+              ),
+
+              // Create Roubded images
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height * 0.50,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    generateImages(
+                      context: context,
+                      location: 'Grand Canyon',
+                      rating: 4.5,
+                      isFav: false,
+                      image: 'image1.jpeg',
+                    ),
+                    generateImages(
+                      context: context,
+                      location: 'New York',
+                      rating: 4.5,
+                      isFav: true,
+                      image: 'image2.jpeg',
+                    ),
+                    generateImages(
+                      context: context,
+                      location: 'Tokyo',
+                      rating: 4.5,
+                      isFav: false,
+                      image: 'image1.jpeg',
+                    ),
+                    generateImages(
+                      context: context,
+                      location: 'Grand Canyon',
+                      rating: 4.5,
+                      isFav: true,
+                      image: 'image2.jpeg',
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
@@ -153,6 +199,125 @@ class MyApp extends StatelessWidget {
           Text(text),
         ],
       ),
+    );
+  }
+
+  Widget generateImages({
+    required BuildContext context,
+    required String location,
+    required double rating,
+    required bool isFav,
+    required String image,
+  }) {
+    IconData? resultIcon;
+
+    if (isFav) {
+      resultIcon = Icons.favorite;
+    } else {
+      resultIcon = Icons.favorite_border;
+    }
+
+    return Stack(
+      children: [
+        Container(
+          height: 500,
+          width: MediaQuery.of(context).size.width * 0.60,
+          alignment: Alignment.centerLeft,
+          margin: const EdgeInsets.symmetric(horizontal: 12),
+          decoration: BoxDecoration(
+            color: Colors.red,
+            borderRadius: BorderRadius.circular(45),
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              image: AssetImage(
+                image,
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          top: 0,
+          left: 0,
+          child: Container(
+            width: 85,
+            height: 50,
+            margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+            padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+            decoration: BoxDecoration(
+              color: Colors.black45,
+              borderRadius: BorderRadius.circular(35),
+            ),
+            child: Row(
+              children: [
+                const Icon(
+                  Icons.star,
+                  color: Colors.white,
+                ),
+                Text(
+                  '$rating',
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    letterSpacing: 1.5,
+                    fontWeight: FontWeight.w100,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Positioned(
+          top: 0,
+          right: 0,
+          child: Container(
+            width: 50,
+            height: 50,
+            margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+            padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+            decoration: BoxDecoration(
+              color: Colors.black45,
+              borderRadius: BorderRadius.circular(35),
+            ),
+            child: Icon(
+              resultIcon,
+              color: Colors.white,
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: 0,
+          child: Container(
+            width: MediaQuery.of(context).size.width * 0.55,
+            height: 60,
+            margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+            padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color: Colors.black45,
+              borderRadius: BorderRadius.circular(35),
+            ),
+            child: Row(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  child: Icon(Icons.pin_drop_outlined,
+                      color: Colors.white, size: 30),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: Text(location,
+                      style: const TextStyle(
+                        color: Colors.white70,
+                        fontSize: 20,
+                        letterSpacing: 1.5,
+                        fontWeight: FontWeight.w100,
+                      )),
+                ),
+              ],
+            ),
+          ),
+        )
+      ],
     );
   }
 }
