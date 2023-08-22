c5575593da860e22c7980a14926a9beace6c339b
Mahammad Bakr <49376835+mahammadbakr@users.noreply.github.com>
2023-08-22 15:43:59 +0300

    create a textfield for the search...

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,73 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+
+  final _formKey  = GlobalKey<FormState>();
+  final searchController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Container(
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
-          padding: const EdgeInsets.symmetric(
-            horizontal: 20,
-            vertical: 40,
-          ),
-          alignment: Alignment.center,
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Text(
-                    'What do you \nwant to  discover?',
+        body: Form(
+          key: _formKey,
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 40,
+            ),
+            alignment: Alignment.center,
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      'What do you \nwant to  discover?',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 30,
+                        letterSpacing: 1.5,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Image.network(
+                      'https://upload.wikimedia.org/wikipedia/commons/e/eb/Rubio_Circle.png',
+                      width: 50,
+                      height: 50,
+                    )
+                  ],
+                ),
+        
+                // Create Search Bar
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 20),
+                  child: TextFormField(
+                    controller: searchController,
+                    validator: (value) => value!.isEmpty ? 'Enter a search term' : null,
+                      decoration: InputDecoration(
+                    hintText: 'Search',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                    prefixIcon:  const Icon(Icons.search),
+                    suffixIcon:  const Icon(Icons.filter_list),
+                  )),
+                ),
+        
+                // Create Categries section
+                const Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'categories',
                     style: TextStyle(
                       color: Colors.black,
                       fontSize: 30,
@@ -34,145 +75,117 @@ class MyApp extends StatelessWidget {
                       fontWeight: FontWeight.w600,
                     ),
                   ),
-                  Image.network(
-                    'https://upload.wikimedia.org/wikipedia/commons/e/eb/Rubio_Circle.png',
-                    width: 50,
-                    height: 50,
-                  )
-                ],
-              ),
-              Container(
-                width: MediaQuery.of(context).size.width,
-                margin: const EdgeInsets.symmetric(vertical: 30),
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                height: 60,
-                decoration: BoxDecoration(
-                    border: Border.all(color: Colors.black12, width: 1.5),
-                    borderRadius: const BorderRadius.all(Radius.circular(25))),
-                child: const Row(
-                  children: [
-                    Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 10),
-                        child: Icon(Icons.search)),
-                    Text('search',
-                        style: TextStyle(
-                          color: Colors.black26,
-                          fontSize: 20,
-                          letterSpacing: 1.5,
-                          fontWeight: FontWeight.w600,
-                        )),
-                    Spacer(),
-                    Icon(Icons.filter_list),
-                  ],
                 ),
-              ),
-
-              // Create Categries section
-              const Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  'categories',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 30,
-                    letterSpacing: 1.5,
-                    fontWeight: FontWeight.w600,
+        
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.08,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: [
+                      generateSomething(
+                          text: 'Name 1',
+                          image:
+                              'https://upload.wikimedia.org/wikipedia/commons/e/eb/Rubio_Circle.png'),
+                      generateSomething(
+                          text: 'name 2',
+                          image:
+                              'https://tricky-photoshop.com/wp-content/uploads/2017/08/final-1.png'),
+                      generateSomething(
+                          text: 'Name 1',
+                          image:
+                              'https://upload.wikimedia.org/wikipedia/commons/e/eb/Rubio_Circle.png'),
+                      generateSomething(
+                          text: 'name 2',
+                          image:
+                              'https://tricky-photoshop.com/wp-content/uploads/2017/08/final-1.png'),
+                      generateSomething(
+                          text: 'Name 1',
+                          image:
+                              'https://upload.wikimedia.org/wikipedia/commons/e/eb/Rubio_Circle.png'),
+                      generateSomething(
+                          text: 'name 2',
+                          image:
+                              'https://tricky-photoshop.com/wp-content/uploads/2017/08/final-1.png'),
+                      generateSomething(
+                          text: 'Name 1',
+                          image:
+                              'https://upload.wikimedia.org/wikipedia/commons/e/eb/Rubio_Circle.png'),
+                      generateSomething(
+                          text: 'name 2',
+                          image:
+                              'https://tricky-photoshop.com/wp-content/uploads/2017/08/final-1.png'),
+                      generateSomething(
+                          text: 'Name 1',
+                          image:
+                              'https://upload.wikimedia.org/wikipedia/commons/e/eb/Rubio_Circle.png'),
+                      generateSomething(
+                          text: 'name 2',
+                          image:
+                              'https://tricky-photoshop.com/wp-content/uploads/2017/08/final-1.png'),
+                    ],
                   ),
                 ),
-              ),
-
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.08,
-                child: ListView(
-                  scrollDirection: Axis.horizontal,
-                  children: [
-                    generateSomething(
-                        text: 'Name 1',
-                        image:
-                            'https://upload.wikimedia.org/wikipedia/commons/e/eb/Rubio_Circle.png'),
-                    generateSomething(
-                        text: 'name 2',
-                        image:
-                            'https://tricky-photoshop.com/wp-content/uploads/2017/08/final-1.png'),
-                    generateSomething(
-                        text: 'Name 1',
-                        image:
-                            'https://upload.wikimedia.org/wikipedia/commons/e/eb/Rubio_Circle.png'),
-                    generateSomething(
-                        text: 'name 2',
-                        image:
-                            'https://tricky-photoshop.com/wp-content/uploads/2017/08/final-1.png'),
-                    generateSomething(
-                        text: 'Name 1',
-                        image:
-                            'https://upload.wikimedia.org/wikipedia/commons/e/eb/Rubio_Circle.png'),
-                    generateSomething(
-                        text: 'name 2',
-                        image:
-                            'https://tricky-photoshop.com/wp-content/uploads/2017/08/final-1.png'),
-                    generateSomething(
-                        text: 'Name 1',
-                        image:
-                            'https://upload.wikimedia.org/wikipedia/commons/e/eb/Rubio_Circle.png'),
-                    generateSomething(
-                        text: 'name 2',
-                        image:
-                            'https://tricky-photoshop.com/wp-content/uploads/2017/08/final-1.png'),
-                    generateSomething(
-                        text: 'Name 1',
-                        image:
-                            'https://upload.wikimedia.org/wikipedia/commons/e/eb/Rubio_Circle.png'),
-                    generateSomething(
-                        text: 'name 2',
-                        image:
-                            'https://tricky-photoshop.com/wp-content/uploads/2017/08/final-1.png'),
-                  ],
+        
+                const SizedBox(
+                  height: 20,
                 ),
-              ),
-
-              const SizedBox(
-                height: 20,
-              ),
-
-              // Create Roubded images
-              SizedBox(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height * 0.50,
-                child: ListView(
-                  scrollDirection: Axis.horizontal,
-                  children: [
-                    generateImages(
-                      context: context,
-                      location: 'Grand Canyon',
-                      rating: 4.5,
-                      isFav: false,
-                      image: 'image1.jpeg',
-                    ),
-                    generateImages(
-                      context: context,
-                      location: 'New York',
-                      rating: 4.5,
-                      isFav: true,
-                      image: 'image2.jpeg',
-                    ),
-                    generateImages(
-                      context: context,
-                      location: 'Tokyo',
-                      rating: 4.5,
-                      isFav: false,
-                      image: 'image1.jpeg',
-                    ),
-                    generateImages(
-                      context: context,
-                      location: 'Grand Canyon',
-                      rating: 4.5,
-                      isFav: true,
-                      image: 'image2.jpeg',
-                    ),
-                  ],
+        
+                // Create Roubded images
+                SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height * 0.50,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: [
+                      generateImages(
+                        context: context,
+                        location: 'Grand Canyon',
+                        rating: 4.5,
+                        isFav: false,
+                        image: 'image1.jpeg',
+                      ),
+                      generateImages(
+                        context: context,
+                        location: 'New York',
+                        rating: 4.5,
+                        isFav: true,
+                        image: 'image2.jpeg',
+                      ),
+                      generateImages(
+                        context: context,
+                        location: 'Tokyo',
+                        rating: 4.5,
+                        isFav: false,
+                        image: 'image1.jpeg',
+                      ),
+                      generateImages(
+                        context: context,
+                        location: 'Grand Canyon',
+                        rating: 4.5,
+                        isFav: true,
+                        image: 'image2.jpeg',
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+
+                const SizedBox(
+                  height: 20,
+                ),
+
+                // Create a button
+                SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  height: 50,
+                  child: ElevatedButton(
+                    onPressed: () {
+                     _formKey.currentState!.validate();
+                    },
+                    child: const Text('Submit'),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
